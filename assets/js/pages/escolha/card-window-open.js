fde67223379b7b65d4e76bc769375d6c0a20cad3
Bianca Almeida <151306147+biancalmds@users.noreply.github.com>
2025-05-04 00:37:53 -0300

    adicionando favicon e trabalhando na navegação das páginas com js

---
+++ assets/js/pages/escolha/card-window-open.js
@@ -0,0 +1,11 @@
+const cardAluno = () => {
+    window.location.href = './alunos.html'
+}
+
+const cardTurma = () => {
+    window.location.href = './turmas.html'
+}
+
+const cardProfessor = () => {
+    window.location.href = './professores.html'
+}
